9210a91219076d40a882cc4807171914ba3ff66e
YasserMuhammad <myasser612@gmail.com>
2018-01-08 00:59:01 +0200

    Fix Build

--- src/pages/quote/quote.ts
+++ src/pages/quote/quote.ts
@@ -1,14 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the QuotePage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-quote',
   templateUrl: 'quote.html',
@@ -16,10 +8,6 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 export class QuotePage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad QuotePage');
   }
 
 }
