d97afadfafc9bc7b0ad83f109ebc17678bc8c24e
YasserMuhammad <myasser612@gmail.com>
2018-01-07 23:30:38 +0200

    Add List in Libirary Page

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -1,25 +1,18 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component, OnInit } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { Quote } from '../data/data.interface';
+import quotes from '../data/quotes';
 
-/**
- * Generated class for the LibraryPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
-@IonicPage()
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
-export class LibraryPage {
+export class LibraryPage implements OnInit{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  quoteCollection: {category: string, quote:Quote[] , icon:string}[];
+
+  ngOnInit(){
+    this.quoteCollection = quotes;
+    console.log(this.quoteCollection)
   }
-
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad LibraryPage');
-  }
-
 }
